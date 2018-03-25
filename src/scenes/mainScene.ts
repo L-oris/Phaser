7f82f0d5152d4ea27c9e5b53eb65b2b9c6d1f98f
Loris <loris4526@gmail.com>
2018-03-25 15:21:08 +0200

    Move player creation in MainScene, and add collision check

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,24 +1,40 @@
 export class MainScene extends Phaser.State {
+  player: Phaser.Sprite
+  platforms: Phaser.Group
+
   preload(): void {
     this.load.image('sky', 'public/assets/img/sky.png')
     this.load.image('ground', 'public/assets/img/platform.png')
     this.load.image('star', 'public/assets/img/star.png')
+    this.load.spritesheet('dude', 'public/assets/img/dude.png', 32, 48)
   }
 
   create(): void {
     this.physics.startSystem(Phaser.Physics.ARCADE)
     this.add.sprite(0, 0, 'sky')
 
-    const platforms = this.add.group()
-    platforms.enableBody = true
+    this.platforms = this.add.group()
+    this.platforms.enableBody = true
 
-    const ground = platforms.create(0, this.world.height - 64, 'ground')
+    const ground = this.platforms.create(0, this.world.height - 64, 'ground')
     ground.scale.setTo(2, 2)
     ground.body.immovable = true
 
-    platforms.create(400, 400, 'ground').body.immovable = true
-    platforms.create(-150, 250, 'ground').body.immovable = true
+    this.platforms.create(400, 400, 'ground').body.immovable = true
+    this.platforms.create(-150, 250, 'ground').body.immovable = true
+
+    this.player = this.add.sprite(32, this.world.height - 150, 'dude')
+    this.physics.arcade.enable(this.player)
+
+    this.player.body.bounce.y = 0.2
+    this.player.body.gravity.y = 300
+    this.player.body.collideWorldBounds = true
+
+    this.player.animations.add('left', [0, 1, 2, 3], 10, true)
+    this.player.animations.add('right', [5, 6, 7, 8], 10, true)
   }
 
-  update(): void {}
+  update(): void {
+    const hitPlatform = this.physics.arcade.collide(this.player, this.platforms)
+  }
 }
